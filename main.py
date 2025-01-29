0f7e16f69da1c02cae672c418d9991acbfd2733c
Leandro S Silva <bhetamed@gmail.com>
2025-01-29 15:56:03 -0300

    test(tests): testes

--- main.py
+++ main.py
@@ -1,9 +1,106 @@
-from scripts.extract.exportar_dados_para_parquet import salvar_dataframe_em_parquet
+from scripts.extract.exportar_dados_para_parquet import salvar_dados
 from scripts.load.load import load_dataframe
+from config.configs import settings
 import pandas as pd
 
 
 query ="""
+SELECT
+    AVA.RA,
+    PP.NOME AS ALUNO,
+    MPL.CODTURMA,
+    STD.CODTURMA AS CODTURMA_2,
+    ATD.IDTURMADISC,
+    STD.CODDISC,
+    SD.NOME AS DISCIPLINA,
+    ETP.ETAPA AS ETAPA_PAI,
+    ETP.DESCRICAO AS DESC_ETAPA_PAI,
+	ETP.LABEL as SEMESTRE,
+    AV.NOME AS AVALIACAO,
+    AV.TIPO AS TIPO_AVALIACAO,
+    AV.PESO,
+    AV.NOTAMAXIMA ,
+    ATD.DATA AS DATA_APL_PROVA,
+    AVA.NOTA AS NOTA_ALUNO,
+    CONCAT('https://diario.escolamobile.com.br/php/imagem_carometro.php?ra=',AVA.RA) AS FOTO,
+    GETDATE() as 'ULTIMA_ATUALIZACAO',
+    case
+		when lic.PERCFEITAS is null then '-'
+		else lic.PERCFEITAS  
+	end as 'LICAO_DE_CASA',
+	CASE	
+		WHEN STD.CODDISC LIKE '%.I' THEN 'Interdisciplinar'
+		WHEN STD.CODDISC LIKE '%.E' THEN 'Eletiva'
+		WHEN (STD.CODDISC = 'ING' OR STD.CODDISC = 'ESP') THEN 'Turma Mista'
+	ELSE 'Regular'
+		END AS TIPO_DISCIPLINA,
+        ET.DESCRICAO AS PERIODO,
+        AV.PUBLICAR,
+		SPL.CODPERLET as 'ANO_LETIVO',
+		CASE
+			WHEN AV.ULTIMOPERIODO IS NULL THEN 0
+			ELSE AV.ULTIMOPERIODO
+		END AS 'ULTIMOPERIODO'
+    -- falt.FALTAS,s
+    -- falt.FALTASSTR
+   FROM
+    DiarioMB.dbo.ETAPAS ET
+    INNER JOIN DiarioMB.dbo.ETAPAS ETP ON ETP.ETAPA = ET.ETAPAPAI
+    INNER JOIN DiarioMB.dbo.AVALIACOES AV ON AV.ETAPA = ET.ETAPA
+    INNER JOIN DiarioMB.dbo.AVALIACOESTURMASDISC ATD ON ATD.AVALIACAO = AV.AVALIACAO
+    INNER JOIN DiarioMB.dbo.AVALIACOESALUNOS AVA ON AVA.AVALIACAO = AV.AVALIACAO
+    INNER JOIN CorporeRM.dbo.SMATRICULA MAT ON MAT.CODCOLIGADA = ATD.CODCOLIGADA AND MAT.IDTURMADISC = ATD.IDTURMADISC AND MAT.RA = AVA.RA
+    INNER JOIN CorporeRM.dbo.SMATRICPL MPL ON MPL.CODCOLIGADA = MAT.CODCOLIGADA AND MPL.IDPERLET = MAT.IDPERLET AND MPL.IDHABILITACAOFILIAL = MAT.IDHABILITACAOFILIAL AND MPL.RA = MAT.RA
+    INNER JOIN CorporeRM.dbo.SALUNO SA ON SA.CODCOLIGADA = MPL.CODCOLIGADA AND SA.RA = MPL.RA
+    INNER JOIN CorporeRM.dbo.PPESSOA PP ON PP.CODIGO = SA.CODPESSOA
+    INNER JOIN CorporeRM.dbo.SPLETIVO SPL ON SPL.CODCOLIGADA = MPL.CODCOLIGADA AND SPL.IDPERLET = MPL.IDPERLET
+    INNER JOIN CorporeRM.dbo.SSTATUS SST ON SST.CODCOLIGADA = MPL.CODCOLIGADA AND SST.CODSTATUS = MPL.CODSTATUS
+    INNER JOIN CorporeRM.dbo.STURMADISC STD ON STD.CODCOLIGADA = MAT.CODCOLIGADA AND STD.IDTURMADISC = MAT.IDTURMADISC
+    INNER JOIN CorporeRM.dbo.SDISCIPLINA SD ON SD.CODCOLIGADA = STD.CODCOLIGADA AND SD.CODDISC = STD.CODDISC
+    left join DiarioMB.dbo.REL_LICOESALUNOS as lic on lic.RA = ava.RA and lic.CODTURMA = mpl.CODTURMA and lic.CODDISC = std.CODDISC
+    and lic.CODREDUZIDO = etp.CODREDUZIDO
+    -- left join DiarioMB.dbo.VW_FALTASALUNOS as falt on falt.RA = ava.RA and falt.CODTURMA = mpl.CODTURMA and falt.CODDISC = std.CODDISC
+   WHERE
+   1=1
+    -- AV.PUBLICAR = 1
+    AND SPL.CODPERLET >= 2023
+    and MPL.CODTURMA not like 'C%'
+    and SST.PLATIVO = 'S'
+	and ETP.DESCRICAO like '%Integral%'
+    -- AND ETP.ETAPA = '547' /* 1º Semestre - Médio - Período Parcial */
+    -- and ava.RA = 140365
+    -- and std.CODDISC = 'QUI'
+                ;
+"""
+
+salvar_dados(
+    origem="google_sheets",
+    nome_arquivo="planilha_dados",
+    formato="excel",
+    path_file="data/raw/sheets",
+    api_key=settings.API_GOOGLE_SHEETS,
+    spreadsheet_url="https://docs.google.com/spreadsheets/d/1XE8jyzGV8Mi1XYx5UIL1t4TK4V6YdIh1hOyuSHHm0S8"
+)
+print("Planilha salva com sucesso!")
+
+# Chamada da função
+salvar_dados(
+    origem='sql',
+    nome_arquivo='alunos_teste',
+    formato='parquet',
+    path_file='data/raw/parquet',
+    query=query,
+    credenciais={'server':settings.MS_SQL_SERVER, 
+                 'user':settings.MS_SQL_USER, 
+                 'password':settings.MS_SQL_PASSWORD},
+    nome_banco='corporeRM',
+    tipo_banco='sqlserver'
+)
+print("Dados salvos com sucesso!")
+
+
+
+query_mysql = """
 SELECT
 	STU.identification AS 'ALUNO_RA',
     STU.name AS 'ALUNO_NOME',
@@ -80,65 +177,23 @@ ORDER BY
 -- LIMIT 100
 ;
 """
-nome_arquivo = "corretor"
-credenciais = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'root',
-    'port': '3306'
-}
-#salvar_dataframe_em_parquet(query, nome_arquivo, credenciais, 'escola_mobile_corretor', 'mysql')
-
-# Criação do DataFrame de teste
-data = {
-    "id": [1, 2, 3],
-    "nome": ["João", "Maria", "José"],
-    "idade": [25, 30, 22],
-    "cidade": ["São Paulo", "Rio de Janeiro", "Belo Horizonte"]
-}
-
-df_teste = pd.read_parquet("data/raw/parquet/corporeRM.consulta_usuarios.parquet")
-
-
-load_dataframe(
-    destino='mysql',
-    dataframe=df_teste,
-    target_table='corretor',
-    credenciais=credenciais,
-    nome_banco='teste',
-    if_exists='replace')
-print("Dados carregados com sucesso!")
-
-
-
-import pandas as pd
-
-# Dados de exemplo em um DataFrame
-data = {
-    "id": [1, 2, 3],
-    "nome": ["Alice", "Bob", "Charlie"],
-    "idade": [25, 30, 35]
-}
-df = pd.DataFrame(data)
-
-# Credenciais do BigQuery
-credenciais = {
-    "project_id": "seu-projeto-id",  # Substitua pelo ID do seu projeto no Google Cloud
-    "credentials_path": "caminho/para/suas/credenciais.json"  # Substitua pelo caminho do arquivo JSON de credenciais
-}
-
-# Nome da tabela no BigQuery (no formato dataset.tabela)
-target_table = "seu_dataset.sua_tabela"
 
 # Chamada da função
-try:
-    load_dataframe(
-        destino="bigquery",
-        dataframe=df,
-        target_table=target_table,
-        credenciais=credenciais,
-        if_exists="replace"  # Substitua por 'append' se quiser adicionar dados à tabela existente
-    )
-    print("Dados carregados com sucesso no BigQuery!")
-except Exception as e:
-    print(f"Erro ao carregar os dados: {e}")
+salvar_dados(
+    origem='sql',
+    nome_arquivo='teste_mysql',
+    formato='parquet',
+    path_file='data/raw/parquet',
+    query=query_mysql,
+    credenciais={
+        'user': settings.MYSQL_USER,
+        'password': settings.MYSQL_PASSWORD,
+        'host': settings.MYSQL_HOST,
+        'port': settings.MYSQL_PORT
+    },
+    nome_banco='escola_mobile_corretor',
+    tipo_banco='mysql'
+)
+
+print(pd.read_parquet('data/raw/parquet/teste_mysql.parquet'))
+print(pd.read_parquet('data/raw/parquet/alunos_teste.parquet'))
